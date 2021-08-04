f0f7c354cc6158fa31194917e1e5de8e60f01b07
kb829 <kbpark829@gmail.com>
2021-08-04 18:26:24 +0900

    springboot init

---
+++ toy-spring/src/main/java/toy/toyspring/ToySpringApplication.java
@@ -0,0 +1,13 @@
+package toy.toyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToySpringApplication.class, args);
+	}
+
+}
---
+++ toy-spring/src/test/java/toy/toyspring/ToySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.toyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
