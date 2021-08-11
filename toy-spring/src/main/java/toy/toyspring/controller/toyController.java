4bc99ceda9a264c98339c1b9f1287bef58296b1e
kb829 <kbpark829@gmail.com>
2021-08-11 17:47:51 +0900

    welcome page init

---
+++ toy-spring/src/main/java/toy/toyspring/controller/toyController.java
@@ -0,0 +1,15 @@
+package toy.toyspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class toyController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello");
+        return "hello";
+    }
+}
